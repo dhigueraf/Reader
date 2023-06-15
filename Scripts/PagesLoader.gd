2136c1f16c9332c221381130c3372f98707827bf
Diego Leonardo Higuera Fernández <dhiguerafernandez@gmail.com>
2023-06-15 02:21:35 -0400

    Add advance an previous buttons

--- Scripts/PagesLoader.gd
+++ Scripts/PagesLoader.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 var pages = []
 var actualindex = 0
+var maxindex = 1
 
 func _ready():
 	var path = OS.get_executable_path()
@@ -24,12 +25,43 @@ func _ready():
 	else:
 		print("An error occurred when trying to access the path.")
 		
-		
-	var image = Image.new()
 	if(pages.size() > 0):
-		var err = image.load(pages[actualindex])
-		var texture = ImageTexture.new()
-		texture.create_from_image(image, 0)
-		print(texture)
-		$ScrollContainer/TextureRect.texture = texture
+		maxindex = pages.size()
+		updateindex(0)
+	else:
+		maxindex = 0
 
+func setimage(index):
+	print("set iamge index: " + str(index))
+	var image = Image.new()
+	var err = image.load(pages[index])
+	var texture = ImageTexture.new()
+	texture.create_from_image(image, 0)
+	print(texture)
+	$ScrollContainer/TextureRect.texture = texture
+	
+	$EtiquetaPagina.text = "Notas pagina " + str(index)
+
+func updateindex(num):
+	print("actualindex " + str(actualindex) )
+	actualindex += num
+
+	if num == 1:
+		$retroceder.disabled = false
+		if actualindex > maxindex -2:
+			actualindex = maxindex -1
+			$avanzar.disabled = true
+	elif num == -1:
+		$avanzar.disabled = false
+		if actualindex < 1:
+			actualindex = 0
+			$retroceder.disabled = true
+	
+	setimage(actualindex)	
+
+
+func _on_retroceder_pressed():
+	updateindex(-1)
+
+func _on_avanzar_pressed():
+	updateindex(1)
